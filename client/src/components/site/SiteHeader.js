4311e4cf85c70a11a134daca525f7b2321b81c4e
cloudvision20 <77039666+cloudvision20@users.noreply.github.com>
2023-10-04 08:36:13 +0800

    siteheader

--- client/src/components/site/SiteHeader.js
+++ client/src/components/site/SiteHeader.js
@@ -1,15 +1,33 @@
-import { useEffect } from 'react'
-import { useNavigate, Link, useLocation } from 'react-router-dom'
+
 import SiteHeaderForm from './SiteHeaderForm'
+import useTitle from "../../hooks/useTitle"
 
 
 const SiteHeader = () => {
+    useTitle('Site Page')
+
+   // const { id } = useParams()
+
+   
+
+    //const { data: res, isSuccess } = useGetActivityByIdQuery(id);
+
+    let content
+
+  //  if (isSuccess) {
+
+       // if (!res.activity || !res.users?.length) return <PulseLoader color={"#FFF"} />
+
+       // if (!isManager && !isAdmin) {
+       //     if (res.activity.userId.username !== username) {
+       //         return <p className="errmsg">No access</p>
+        //    }
+        //}
+        content = <SiteHeaderForm />
+       
+   // }
+
   
-
-    const content = (
-        <SiteHeaderForm></SiteHeaderForm>
-    )
-
     return content
 }
 export default SiteHeader
